9d202a3e52fc8bbf22446bd8d3db3dadb7bdb720
lucabecci <beccibrian@gmail.com>
2021-04-17 20:47:25 -0300

    chore(@vars/vars.rb): Vars reference with diferents asignations added

---
+++ vars/vars.rb
@@ -0,0 +1,17 @@
+# definition of var
+my_name = "Luca"
+
+# definition of expresion
+var_expr = ((32 - 20) / 2) ** 2
+puts "var_expr ===> #{var_expr}"
+
+# assing diferents values in one var
+var_changed = 20
+var_changed = 22
+
+# diferents definition
+normal_var = 20
+normal_var += 2 #added 
+normal_var = 3 #substract
+normal_var *= 2 #multiply
+normal_var /= 2 # divided

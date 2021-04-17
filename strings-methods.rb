ba4ce99eafb9268cf7889929bddde4bc9e84d979
lucabecci <beccibrian@gmail.com>
2021-04-17 20:25:19 -0300

    chore(@strings-method): Strings method added with multiple native funcs

---
+++ strings-methods.rb
@@ -0,0 +1,40 @@
+# capitalize
+checkCapitalize = "hello"
+puts "capitalize ===>: #{checkCapitalize.capitalize}"
+
+# include 
+puts "includes [ing] ===>: #{"amazing".include?("ing")}"
+
+# uppercase
+checkUpc = "toupper"
+puts "uppercase ===>: #{checkUpc.upcase}"
+
+# lowercase 
+checkLowc = "LOWERCASE"
+puts "lowercase ===>: #{checkLowc.downcase}"
+
+# empty?
+puts "empty? ===>: #{"emptyFalse".empty?}"
+
+# length
+puts "length ===>: #{"lolo".length}"
+
+# reverse 
+puts "reverse str ===>: #{"hello world".reverse}"
+
+# split 
+puts "split ===>: #{"splited".split("")}"
+
+# trim
+puts "strip ===> #{"  trimed            ".strip}"
+
+
+"he77o".sub("7", "l")           #=> "hel7o"
+
+"he77o".gsub("7", "l")          #=> "hello"
+
+"hello".insert(-1, " dude")     #=> "hello dude"
+
+"hello world".delete("l")       #=> "heo word"
+
+"!".prepend("hello, ", "world") #=> "hello, world!"
